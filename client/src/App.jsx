5ad9d5f6337a0179eb85ef53e99d16b850c9c2d6
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-08-16 16:04:40 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, } from 'react-router-dom'
 import Home from "./Page/Home"
 import Header from "./Components/Header"
 import Footer from "./Components/Footer"
+import Theree from "./Page/Theree"
 
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
     <Header/>
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route  path="/test" element={<Theree/>} />
     </Routes>
     <Footer/>
     </>
