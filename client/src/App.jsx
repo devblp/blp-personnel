ff1ce3c2686b9df953095094bd4437f117278896
SinaNasibparast <153799940+devblp@users.noreply.github.com>
2024-08-17 16:15:46 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import { Routes, Route, } from 'react-router-dom'
-import Home from "./Page/Home"
-import Header from "./Components/Header"
-import Footer from "./Components/Footer"
-import Theree from "./Page/Theree"
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Page/Home";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Main from "./Page/Main";
+const Layout = () => (
+  <>
+    <Header />
+    <Home />
+    <Footer />
+  </>
+);
 
 export default function App() {
   return (
     <>
-    <Header/>
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route  path="/test" element={<Theree/>} />
-    </Routes>
-    <Footer/>
+      <Routes>
+        <Route path="/Home" element={<Layout />}>
+        </Route>
+        <Route exact path="/" element={<Main />} />
+      </Routes>
     </>
-  )
+  );
 }
